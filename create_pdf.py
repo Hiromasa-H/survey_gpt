2aed496d1343ae5be398065811f7f3fbb3fa1ba8
Hiromasa-H <hhiromasa217@gmail.com>
2023-09-13 13:43:04 +0900

    updated layout

--- create_pdf.py
+++ create_pdf.py
@@ -5,6 +5,8 @@ from reportlab.pdfbase import pdfmetrics
 from reportlab.pdfbase.cidfonts import UnicodeCIDFont
 import math
 import textwrap
+import pickle
+import datetime
 
 def make_pdf(title_text,subtitle_text,date_affiliation,midashi_list,honbun_list):#,im_1_file_list,im_2_file_list):
     # 画像変形関数
@@ -18,7 +20,7 @@ def make_pdf(title_text,subtitle_text,date_affiliation,midashi_list,honbun_list)
         return mag
 
     # オブジェクトの生成、用紙設定
-    c = canvas.Canvas('presentation.pdf')  # pdfファイルの生成 
+    c = canvas.Canvas('static/slides/presentation.pdf')  # pdfファイルの生成 
     w, h = 1462, 1033
     c.setPageSize((w,h))  # 1462 x 1033ピクセル(125dpi)設定
     font_name = 'HeiseiKakuGo-W5'  # フォント名
@@ -63,14 +65,14 @@ def make_pdf(title_text,subtitle_text,date_affiliation,midashi_list,honbun_list)
     # 本文ページの設定
     pages = len(midashi_list)  # 本文のページ数
     midashi_font_size = 60  # 見出しの文字サイズ
-    text_font_size = 48  # 本文の文字サイズ
+    text_font_size = 30  # 本文の文字サイズ
     gyokan = 15  # 本文の行間隔\
     honbun_y_pos = 240  # 本文の表示位置(y)
     im_1_area_x, im_1_area_y = 650, 450  # 画像1表示エリアのサイズ
     im_2_area_x, im_2_area_y = 650, 450  # 画像2表示エリアのサイズ
 
 
-    for midashi, honbun, im_1_file, im_2_file in zip(midashi_list, honbun_list, im_1_file_list, im_2_file_list): 
+    for midashi, honbun_dict, im_1_file, im_2_file in zip(midashi_list, honbun_list, im_1_file_list, im_2_file_list): 
         
         c.showPage()  # 改ページ
         
@@ -86,28 +88,63 @@ def make_pdf(title_text,subtitle_text,date_affiliation,midashi_list,honbun_list)
         im_2_mag = image_mag(im_2_x, im_2_y, im_2_area_x, im_2_area_y)
         im_2_r = im_2.resize((int(im_2_x*im_2_mag),int(im_2_y*im_2_mag)))
 
-        # 本文のテキスト折り返し
-        honbun_len = len(honbun)
+        honbun = f"""
+        課題:{honbun_dict['problem']}
+
+        手法:{honbun_dict['method']}
+
+        結果:{honbun_dict['result']}
+        """
+        
         text_area_w = w-2*margin 
 
+        c.drawInlineImage(logo_r,w-logo_r_x-margin,h-logo_r_y-margin)
+
+        # 見出しのテキスト折り返し
+        midashi_len = len(midashi)
+        midashi_chr_w = text_area_w // (midashi_font_size/2)  # 見出しの1行の文字数
+        midashi_rows = math.ceil(midashi_len/midashi_chr_w)  # 見出しの行数
+        midashi_rows_list = textwrap.wrap(midashi, midashi_chr_w)  # 見出しの折り返し
+
+        c.setFont(font_name, midashi_font_size)
+
+        count_row = 0
+        for i in midashi_rows_list:
+            c.drawString(margin, h-90-count_row*(midashi_font_size+gyokan), i)
+            count_row += 1
+
+        # 本文のテキスト折り返し
+        honbun_len = len(honbun)
         text_chr_w = text_area_w // text_font_size  # 本文の1行の文字数
         honbun_rows = math.ceil(honbun_len/text_chr_w)  # 本文の行数
         honbun_rows_list = textwrap.wrap(honbun, text_chr_w) 
-    
-        # 本文ページのレイアウトと描画
-        c.drawInlineImage(logo_r,w-logo_r_x-margin,h-logo_r_y-margin)
-        c.setFont(font_name, midashi_font_size)
-        c.drawString(margin, h-midashi_font_size-margin, midashi)
-        c.setFont(font_name, text_font_size)
 
-        # 本文の折り返し表示
+        c.setFont(font_name, text_font_size)
+        
         count_row = 0
         for i in honbun_rows_list:
             c.drawString(margin, h-honbun_y_pos-count_row*(text_font_size+gyokan), i)
             count_row += 1
 
         # 図の表示
-        c.drawInlineImage(im_1_r,margin,margin)
-        c.drawInlineImage(im_2_r,w/2+margin,margin)
+        # c.drawInlineImage(im_1_r,margin,margin)
+        # c.drawInlineImage(im_2_r,w/2+margin,margin)
 
     c.save()  # ファイル保存
+
+if __name__ == '__main__':
+    # load pickle
+    with open('info_dict.pickle', 'rb') as f:
+        return_list = pickle.load(f)
+    today = datetime.date.today()
+    today_str = today.strftime('%Y%m%d')
+
+    title_list = [ item['title'] for item in return_list ]
+    # body_list = [ "\n".join(item['gpt_summaries'].values()) for item in return_list ]
+    body_list = [ item['gpt_summaries'] for item in return_list ]
+
+    make_pdf(title_text='動画編集サーベイ',
+                subtitle_text='サーベイ実験',
+                date_affiliation=today_str,
+                midashi_list=title_list,
+                honbun_list=body_list)
